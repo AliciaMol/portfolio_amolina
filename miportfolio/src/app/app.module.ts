59699d2708b10484c3919476d92e5ce58426fb34
AliciaMol <54749534+AliciaMol@users.noreply.github.com>
2022-05-27 20:12:39 -0300

    Skills + projects + footer
    Desarrollo de los componentes
    Skills
    Projects
    Footer

--- miportfolio/src/app/app.module.ts
+++ miportfolio/src/app/app.module.ts
@@ -9,6 +9,10 @@ import { HeaderbannerComponent } from './components/headerbanner/headerbanner.co
 import { AboutComponent } from './components/about/about.component';
 import { ExperienceComponent } from './components/experience/experience.component';
 import { EducationComponent } from './components/education/education.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -18,11 +22,28 @@ import { EducationComponent } from './components/education/education.component';
     HeaderbannerComponent,
     AboutComponent,
     ExperienceComponent,
-    EducationComponent
+    EducationComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({
+      radius: 50,
+      showUnits: false,
+      showSubtitle: false,
+      titleFontSize: "12",
+      imageHeight: 50,
+      imageWidth: 50,
+      outerStrokeWidth: 7,
+      innerStrokeWidth: 5,
+      backgroundOpacity: 0.3,
+      backgroundGradient: true,
+      animation: true,
+      animationDuration: 300,
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
