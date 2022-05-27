ed31f4adbe88444a293acd8898eef1e34bd1de67
AliciaMol <54749534+AliciaMol@users.noreply.github.com>
2022-05-27 08:34:34 -0300

    Header + About + Experience + Education
    Desarrollo de los componentes:
    -Header (con la barra de navegación y el banner)
    -About
    -Experience
    -Education

--- miportfolio/src/app/app.module.ts
+++ miportfolio/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HeaderbarnavComponent } from './components/headerbarnav/headerbarnav.component';
+import { HeaderbannerComponent } from './components/headerbanner/headerbanner.component';
+import { AboutComponent } from './components/about/about.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { EducationComponent } from './components/education/education.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeaderbarnavComponent,
+    HeaderbannerComponent,
+    AboutComponent,
+    ExperienceComponent,
+    EducationComponent
   ],
   imports: [
     BrowserModule,
---
+++ miportfolio/src/app/components/headerbarnav/headerbarnav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-headerbarnav',
+  templateUrl: './headerbarnav.component.html',
+  styleUrls: ['./headerbarnav.component.css']
+})
+export class HeaderbarnavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
